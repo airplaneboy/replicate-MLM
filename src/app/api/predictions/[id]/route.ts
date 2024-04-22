37f5429121cae60517fa55585af1362ad6f73a88
Sulaiman Agara <airplaneboy00@gmail.com>
2024-04-22 19:13:41 -0400

    Made get predictions route catch errors properly

--- src/app/api/predictions/[id]/route.ts
+++ src/app/api/predictions/[id]/route.ts
@@ -1,22 +1,26 @@
-import Replicate from "replicate";
+import { NextResponse } from 'next/server';
+import Replicate from 'replicate';
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN
+  auth: process.env.REPLICATE_API_TOKEN,
 });
 
-export async function GET(request: Request,
-  { params }: { params: { id: string } }) {
-  const prediction = await replicate.predictions.get(params.id);
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  try {
+  } catch (error: any) {
+    const errorMessage = error.message;
 
-  if (prediction?.error) {
-    return new Response(
-      JSON.stringify({ detail: prediction.error.detail }),
+    //Server returns error in a weird way where it's mixed with a bunch of strings, so I have to extract and parse it
+    return NextResponse.json(
+      { error: JSON.parse(errorMessage.substring(errorMessage.indexOf('{'), errorMessage.lastIndexOf('}') + 1)) },
       { status: 500 }
     );
   }
+  const prediction = await replicate.predictions.get(params.id);
 
-  return new Response(
-    JSON.stringify(prediction),
-    { status: 200 }
-  );
+  if (prediction?.error) {
+    return new Response(JSON.stringify({ error: { detail: prediction.error.detail } }), { status: 500 });
+  }
+
+  return new Response(JSON.stringify(prediction), { status: 200 });
 }
