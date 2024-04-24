f84fcd5a6b1a63220db29f34eb5d8f46a8008f5a
Sulaiman Agara <airplaneboy00@gmail.com>
2024-04-24 19:46:20 -0400

    Fixed darkmode issue

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -24,14 +24,11 @@ export const HeroHighlight = ({
   }
   return (
     <div
-      className={cn(
-        'relative h-[40rem] flex items-center bg-white dark:bg-black justify-center w-full group',
-        containerClassName
-      )}
+      className={cn('relative h-[40rem] flex items-center bg-black justify-center w-full group', containerClassName)}
       onMouseMove={handleMouseMove}>
-      <div className='absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none' />
+      <div className='absolute inset-0 bg-dot-thick-neutral-800  pointer-events-none' />
       <motion.div
-        className='pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100'
+        className='pointer-events-none bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100'
         style={{
           WebkitMaskImage: useMotionTemplate`
             radial-gradient(
@@ -84,7 +81,7 @@ export const Highlight = ({
         display: 'inline',
       }}
       className={cn(
-        `relative inline-block pb-1 px-1 rounded-lg bg-gradient-to-r from-green-400 via-purple-700 to-blue-400 dark:from-indigo-500 dark:to-purple-500`,
+        `relative inline-block pb-1 px-1 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-700 to-purple-500`,
         className
       )}>
       {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,6 +38,9 @@ const config: Config = {
       );
     },
   ],
+  daisyui: {
+    themes: ['dark'],
+  },
 };
 
 export default config;
